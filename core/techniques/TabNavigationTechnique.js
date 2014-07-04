4b3916d515fcdd589340af5d4b6beee46532cb64
Alexander Henka <Alexander.Henka@googlemail.com>
2014-07-04 15:18:57 +0200

    Use domElement in tabNavigaationTechnique

--- core/techniques/TabNavigationTechnique.js
+++ core/techniques/TabNavigationTechnique.js
@@ -10,7 +10,7 @@
  * 'TAB' navigation technique to find the element, defined as 'domElement'.
  * @type {navigation}
  */
-module.exports = tabNavigationTechnique = function (tagName,ele) {
+module.exports = tabNavigationTechnique = function (domElement) {
     var that = this,
         _by = that.webdriver.By;
 
@@ -57,7 +57,7 @@ module.exports = tabNavigationTechnique = function (tagName,ele) {
     };
 
 
-    var helperFunction = function (ele) {
+    var helperFunction = function (domElement) {
         return action_to_be_performed.perform()
             .then(function () {
                 if(webElement === undefined){
@@ -65,18 +65,7 @@ module.exports = tabNavigationTechnique = function (tagName,ele) {
                 }else{
                     that.webdriver.WebElement.equals(webElement,that.driver.switchTo().activeElement()).then(function (eq) {
                         if(eq){
-                            var _errorText;
-                            switch (true){
-                                case (ele.href && !(ele.id && ele.text)):
-                                    _errorText = 'Element with href= '+ele.href+'cloud not be found or reached!';
-                                    break;
-                                case (ele.id && !(ele.href && ele.text)):
-                                    _errorText = 'Element with id= '+ele.id+'cloud not be found or reached!';
-                                    break;
-                                case (ele.text && !(ele.id && ele.href)):
-                                    _errorText = 'Element with text= '+ele.text+'cloud not be found or reached!';
-                                    break;
-                            }
+                            var _errorText = 'Element with' +domElement.getSearchAttributeName()+'= '+domElement.getSearchAttributeValue()+'cloud not be found or reached!';
                             return deferred.reject(new Error(_errorText));
                         }
 
@@ -87,46 +76,31 @@ module.exports = tabNavigationTechnique = function (tagName,ele) {
                 return that.driver.switchTo().activeElement();
             })
             .then(function (activeElement) {
-                switch (true){
-                    case (ele.href && !(ele.id && ele.text)):
-                        if (isAttributePresent(activeElement, 'href')) {
-                            return activeElement.getAttribute('href')
-                                .then(function (text) {
-                                    return (text === ele.href) ? activeElement : helperFunction(ele);
-                                });
 
-                        } else {
-                            return helperFunction(ele);
-                        }
-                        break;
-                    case (ele.id && !(ele.href && ele.text)):
-                        if (isAttributePresent(activeElement, 'id')) {
-                            return activeElement.getAttribute('id')
-                                .then(function (text) {
-                                    return (text === ele.id) ? activeElement : helperFunction(ele);
-                                });
+                if (domElement.getSearchAttributeValue() === 'text'){
+                    if (hasTextNode(activeElement)) {
+                        return activeElement.getText()
+                            .then(function (text) {
+                                return (text === domElement.getSearchAttributeValue()) ? activeElement : helperFunction(domElement);
+                            });
+                    } else {
+                        return helperFunction(domElement);
+                    }
 
-                        } else {
-                            return helperFunction(ele);
-                        }
-                        break;
-                    case (ele.text && !(ele.id && ele.href)):
-                        if (hasTextNode(activeElement)) {
-                            return  activeElement.getText()
-                                .then(function (text) {
-                                    return (text === ele.text) ? activeElement : helperFunction(ele);
-                                });
+                }else{
+                    if (isAttributePresent(activeElement, domElement.getSearchAttributeName())) {
+                        return activeElement.getAttribute(domElement.getSearchAttributeName())
+                            .then(function (text) {
+                                return (text === domElement.getSearchAttributeValue()) ? activeElement : helperFunction(domElement);
+                            });
 
-                        } else {
-                            return helperFunction(ele);
-                        }
-                        break;
-                    default :
-                        return deferred.reject(new Error('Element with href= '+ele+' not found!'));
-                        break;
+                    } else {
+                        return helperFunction(domElement);
+                    }
+
                 }
             })
     };
-    return helperFunction(ele);
+    return helperFunction(domElement);
 
 };
