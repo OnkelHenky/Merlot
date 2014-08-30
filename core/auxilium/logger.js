ee17358689a463abe6fc7e77c8907bf707cdbfd8
Alexander Henka <Alexander.Henka@googlemail.com>
2014-08-30 08:38:28 +0200

    Minor Changes

--- core/auxilium/logger.js
+++ core/auxilium/logger.js
@@ -3,21 +3,6 @@
  * Copyright by Alexander Henka
  */
 
-/*
- var sys = require('sys'),
- style = require('./lib/colored');
-
- sys.puts("Colored.js is totally " + style.extras.underline(style.foreground.red("rockin'")));
- sys.puts("Colored.js is totally " + style.foreground.black(style.background.yellow("rockin'")));
-
- // Extra credit
- for(var f in style.foreground) {
- String.prototype[f] = style.foreground[f];
- }
-
- sys.puts("greener grass!".green());
- */
-
 var Logger,
     Colored = require('../../lib/colored');
     Merlot = require('../Merlot').Merlot;
